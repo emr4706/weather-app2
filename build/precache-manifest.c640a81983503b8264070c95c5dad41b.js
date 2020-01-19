1ccb6ce3bee2cf584f5f1a07a481cd4701dd5e71
emr4706 <emr4706@gmail.com>
2020-01-19 15:13:49 +0100

    add deployment

---
+++ build/precache-manifest.c640a81983503b8264070c95c5dad41b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0996960b0ff1f6041b2accb0d50567aa",
+    "url": "/weather-app2/index.html"
+  },
+  {
+    "revision": "f446086d74c6d1cef36e",
+    "url": "/weather-app2/static/css/main.67a49c2d.chunk.css"
+  },
+  {
+    "revision": "bbacc2865e1a13eb34cf",
+    "url": "/weather-app2/static/js/2.e551ff30.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app2/static/js/2.e551ff30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f446086d74c6d1cef36e",
+    "url": "/weather-app2/static/js/main.46a04052.chunk.js"
+  },
+  {
+    "revision": "c22aaa3044eb4f617678",
+    "url": "/weather-app2/static/js/runtime-main.19aa83b9.js"
+  }
+]);
